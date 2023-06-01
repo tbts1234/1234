e768aa112c69c073f2eb403b6ec89dc40670af44
tbts1234 <134499184+tbts1234@users.noreply.github.com>
2023-06-01 10:04:22 +0530

    Delete Lp2 directory

--- Lp2/domjqury/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // Add a click event handler to the update button
-    $('#updateButton').click(function() {
-      // Update the text of the message heading
-      $('#message').text('Updated message');
-    });
-  });
